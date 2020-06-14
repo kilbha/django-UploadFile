ad54798905d7e39f9bf5687cac82732c973878be
bhargav <bhargav.mech12@gmail.com>
2020-06-14 12:53:03 +0530

    AllCreated

--- WebApp/admin.py
+++ WebApp/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from WebApp.models import StdModel
 
 # Register your models here.
+admin.site.register(StdModel)
--- WebApp/models.py
+++ WebApp/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class StdModel(models.Model):
+    StdNo = models.IntegerField()
+    StdName = models.CharField(max_length = 300)
+    StdPic = models.FileField(blank=True,null=True)
+    stdAds = models.CharField(max_length = 300)
